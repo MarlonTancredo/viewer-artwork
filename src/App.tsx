6f8e525da01d5b545cb32d4f6ca89d7e0f3c53dd
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-03-22 20:00:09 +0000

    refactor: limiting fetch api to 5 objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
 
+type Creator = {
+    biography: null;
+    birth_year: string;
+    death_year: string;
+    description: string;
+    extent: null;
+    id: number;
+    name_in_original_language: null;
+    qualifier: null;
+    role: string;
+};
+
 type Art = {
     id: number;
     title: string;
     creation_date: string;
+    creators: Creator[];
     description: string;
     images: { web?: { url: string; width: string; height: string; filesize: string; filename: string } };
 };
@@ -12,7 +25,7 @@ const App = () => {
     const [artList, setArtList] = useState<Art[]>();
 
     const getData = async () => {
-        const response = await fetch("https://openaccess-api.clevelandart.org/api/artworks");
+        const response = await fetch("https://openaccess-api.clevelandart.org/api/artworks/?limit=5");
         const data = await response.json();
         setArtList(data.data.slice(0, 5));
         console.log(response);
@@ -29,11 +42,29 @@ const App = () => {
             {artList?.map((art) => {
                 return (
                     <section key={art.id} style={{ width: "500px" }}>
-                        <img src={art.images.web?.url} alt={art.title} style={{ width: "500px" }} />
+                        <img src={art.images.web?.url} alt={art.title} />
                         <p>
                             <strong>Title: </strong>
                             {art.title}
                         </p>
+                        {art.creators.map((creator) => {
+                            return (
+                                <div key={creator.id}>
+                                    <p>
+                                        <strong>Birth: </strong>
+                                        {creator.birth_year}
+                                    </p>
+                                    <p>
+                                        <strong>Death: </strong>
+                                        {creator.death_year}
+                                    </p>
+                                    <p>
+                                        <strong>Creator: </strong>
+                                        {creator.description}
+                                    </p>
+                                </div>
+                            );
+                        })}
                         <p>
                             <strong>Description:</strong> {art.description}
                         </p>
