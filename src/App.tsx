9c642c6c286c126cc213d3ac9edf6d939f4b8c37
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-03-23 23:13:34 +0000

    feat: implementing react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 type Creator = {
     biography: null;
@@ -22,59 +24,69 @@ type Art = {
 };
 
 const App = () => {
-    const [artList, setArtList] = useState<Art[]>();
+    const [limit, setLimit] = useState(5);
+    const { ref, inView } = useInView({ threshold: 0.4 });
 
     const getData = async () => {
-        const response = await fetch("https://openaccess-api.clevelandart.org/api/artworks/?limit=5");
+        const response = await fetch(`https://openaccess-api.clevelandart.org/api/artworks/?limit=${limit}`);
         const data = await response.json();
-        setArtList(data.data);
         console.log(response);
         console.log(data.data);
+        return data.data;
     };
 
-    useEffect(() => {
-        getData();
-    }, []);
+    const { data, isLoading, refetch } = useQuery({
+        queryKey: ["items"],
+        queryFn: () => getData(),
+    });
 
-    return artList ? (
-        <>
-            <h1>Arts</h1>
-            {artList?.map((art) => {
-                return (
-                    <section key={art.id} style={{ width: "500px" }}>
-                        <img src={art.images.web?.url} alt={art.title} />
-                        <p>
-                            <strong>Title: </strong>
-                            {art.title}
-                        </p>
-                        {art.creators.map((creator) => {
-                            return (
-                                <div key={creator.id}>
-                                    <p>
-                                        <strong>Birth: </strong>
-                                        {creator.birth_year}
-                                    </p>
-                                    <p>
-                                        <strong>Death: </strong>
-                                        {creator.death_year}
-                                    </p>
-                                    <p>
-                                        <strong>Creator: </strong>
-                                        {creator.description}
-                                    </p>
-                                </div>
-                            );
-                        })}
-                        <p>
-                            <strong>Description:</strong> {art.description}
-                        </p>
-                        <br />
-                    </section>
-                );
-            })}
-        </>
-    ) : (
-        <h1>Fetching...</h1>
+    useEffect(() => {
+        if (inView) {
+            setLimit((limit) => (limit += 5));
+            refetch();
+        }
+    }, [inView]);
+
+    return (
+        !isLoading && (
+            <>
+                <h1>Arts</h1>
+                {data?.map((art: Art) => {
+                    return (
+                        <section key={art.id}>
+                            <img src={art.images.web?.url} alt={art.title} />
+                            <p>
+                                <strong>Title: </strong>
+                                {art.title}
+                            </p>
+                            {art.creators.map((creator) => {
+                                return (
+                                    <div key={creator.id}>
+                                        <p>
+                                            <strong>Birth: </strong>
+                                            {creator.birth_year}
+                                        </p>
+                                        <p>
+                                            <strong>Death: </strong>
+                                            {creator.death_year}
+                                        </p>
+                                        <p>
+                                            <strong>Creator: </strong>
+                                            {creator.description}
+                                        </p>
+                                    </div>
+                                );
+                            })}
+                            <p>
+                                <strong>Description:</strong> {art.description}
+                            </p>
+                            <br />
+                        </section>
+                    );
+                })}
+                <div ref={ref}>Loading...</div>
+            </>
+        )
     );
 };
 
