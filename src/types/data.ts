e0826faf254b9e8bc311b8b19b344840baa27be4
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-03-27 13:00:45 +0000

    feat: implementing data type

--- src/types/data.ts
+++ src/types/data.ts
@@ -6,6 +6,7 @@ type Art = {
     id: number;
     title: string;
     creation_date: string;
+    department: string;
     creators: Creator[];
     images: { web?: { url: string; width: string; height: string; filesize: string; filename: string } };
 };
