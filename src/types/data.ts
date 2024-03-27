970aff588a89fb7c4df2e66671efd7d6b2cf4ffa
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-03-27 12:16:59 +0000

    feat: implementing types

---
+++ src/types/data.ts
@@ -0,0 +1,25 @@
+type Data = {
+    data: Art[];
+};
+
+type Art = {
+    id: number;
+    title: string;
+    creation_date: string;
+    creators: Creator[];
+    images: { web?: { url: string; width: string; height: string; filesize: string; filename: string } };
+};
+
+type Creator = {
+    biography: null;
+    birth_year: string;
+    death_year: string;
+    description: string;
+    extent: null;
+    id: number;
+    name_in_original_language: null | string;
+    qualifier: null;
+    role: string;
+};
+
+export type { Art, Data, Creator };
