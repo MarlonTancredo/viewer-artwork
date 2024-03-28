3f4d826013d6dcc55074105094bade9d1431acf5
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-03-28 12:50:37 +0000

    feat: impolementing search field

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,25 +6,27 @@ import Loading from "../components/Loading";
 import "./styles.css";
 
 const App = () => {
-    const [limit, setLimit] = useState(8);
+    const [limit, setLimit] = useState(6);
+    const [searchInput, setSearchInput] = useState("");
     const { ref, inView } = useInView({ threshold: 1 });
-
-    const { data, status, error, isFetching, refetch } = useQuery({
-        queryKey: ["items"],
-        queryFn: () => getData(),
-    });
 
     const getData = async () => {
         try {
             const response = await fetch(
-                `https://openaccess-api.clevelandart.org/api/artworks/?q=ocean&limit=${limit}&has_image=1`,
+                `https://openaccess-api.clevelandart.org/api/artworks/?q=${searchInput}&limit=${limit}&has_image=1`,
             );
             const data = await response.json();
+            console.log(data);
             return data.data;
         } catch (error) {
             console.log(error);
         }
     };
+
+    const { data, status, error, isFetching, refetch } = useQuery({
+        queryKey: ["items"],
+        queryFn: () => getData(),
+    });
 
     useEffect(() => {
         if (inView) {
@@ -32,6 +34,10 @@ const App = () => {
             refetch();
         }
     }, [inView]);
+
+    const handleSearchButton = () => {
+        refetch();
+    };
 
     return status === "pending" ? (
         <div className="app-container">
@@ -41,6 +47,34 @@ const App = () => {
         <div className="app-container">{error.message}</div>
     ) : (
         <div className="app-container">
+            <div>
+                <div
+                    style={{
+                        display: "flex",
+                        position: "fixed",
+                        top: "0",
+                        left: "0",
+                        zIndex: "1",
+                        height: "5vh",
+                        width: "100vw",
+                        backgroundColor: "white",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <input
+                        style={{ width: "30vw", height: "2vh" }}
+                        type="text"
+                        placeholder="Enter..."
+                        onChange={(e) => {
+                            setSearchInput(e.target.value);
+                        }}
+                    ></input>
+                    <button style={{ width: "6vw", height: "2.5vh" }} onClick={handleSearchButton}>
+                        Search
+                    </button>
+                </div>
+            </div>
             <Card data={data} />
             <div ref={ref}>{isFetching && <Loading />}</div>
         </div>
