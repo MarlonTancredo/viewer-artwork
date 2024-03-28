d015682945f6aebfa394c50bf2a90c6da5675a68
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-03-28 15:04:42 +0000

    refactor: refactoring

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,43 +3,45 @@ import "./styles.css";
 
 const Card = ({ data }: Data) => {
     return (
-        <div
-            className="card__container"
-            onClick={() => {
-                console.log("Click");
-            }}
-        >
-            {data?.map((art: Art) => {
-                return (
-                    <section key={art.id} className="card__body card--shadow card--fade-in">
-                        <img src={art.images.web?.url} alt={art.title} className="card__img" />
-                        <p>
-                            <strong>{art.title} </strong>
-                        </p>
-                        <hr />
-                        <p>
-                            <strong>Creation date: </strong>
-                            {art.creation_date}
-                        </p>
-                        <p>
-                            <strong>Department: </strong>
-                            {art.department}
-                        </p>
-                        {art.creators.map((creator) => {
-                            return (
-                                <div key={creator.id}>
-                                    <p>
-                                        <strong>Creator: </strong>
-                                        {creator.description}
-                                    </p>
-                                </div>
-                            );
-                        })}
-                        <br />
-                    </section>
-                );
-            })}
-        </div>
+        <>
+            <div
+                className="card__container"
+                onClick={() => {
+                    console.log("Click");
+                }}
+            >
+                {data?.map((art: Art) => {
+                    return (
+                        <section key={art.id} className="card__body card--shadow card--fade-in">
+                            <img src={art.images.web?.url} alt={art.title} className="card__img" />
+                            <p>
+                                <strong>{art.title} </strong>
+                            </p>
+                            <hr />
+                            <p>
+                                <strong>Creation date: </strong>
+                                {art.creation_date}
+                            </p>
+                            <p>
+                                <strong>Department: </strong>
+                                {art.department}
+                            </p>
+                            {art.creators.map((creator) => {
+                                return (
+                                    <div key={creator.id}>
+                                        <p>
+                                            <strong>Creator: </strong>
+                                            {creator.description}
+                                        </p>
+                                    </div>
+                                );
+                            })}
+                            <br />
+                        </section>
+                    );
+                })}
+            </div>
+        </>
     );
 };
 
