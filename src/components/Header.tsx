2be4b344abd4dae1ee3936b48e43178d46dce34f
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-03 18:43:15 +0100

    refactor: refactoring header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ const Header = ({ children }: Children) => {
     return (
         <>
             <div className="header__container">{children}</div>
+            <div style={{ height: "6rem" }}></div>
         </>
     );
 };
