6c0c1189cb3ed7ca120097bbf103ff382c26190e
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-03 14:33:00 +0100

    style: styling modal

--- src/components/SavedCardModal.tsx
+++ src/components/SavedCardModal.tsx
@@ -11,7 +11,20 @@ const SavedCardModal = ({ isSavedClicked }: IsSavedClicked) => {
     if (isSavedClicked) {
         return (
             <>
-                <span style={{ display: `${display.showDisplay}` }}>Saved!</span>
+                <span
+                    style={{
+                        display: `${display.showDisplay}`,
+                        backgroundColor: "#212529",
+                        color: "#f8f9fa",
+                        padding: "1rem",
+                        position: "fixed",
+                        bottom: "0",
+                        right: "0",
+                        zIndex: "1",
+                    }}
+                >
+                    Saved!
+                </span>
             </>
         );
     }
