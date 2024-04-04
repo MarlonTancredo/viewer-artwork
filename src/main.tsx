0fcedeb96c0508ee6cd529cb769ca2648d74f099
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-04 15:19:23 +0100

    feat: implementing client on main

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
 import "./main.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </BrowserRouter>
     </StrictMode>,
 );
