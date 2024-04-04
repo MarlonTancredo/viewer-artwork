f04120f0878371a67946121db2938f525ba97634
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-04 16:47:58 +0100

    style: styling

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import "./styles.css";
 
 const NavLink = () => {
+    const { pathname } = useLocation();
+
     return (
         <>
             <ul className="nav-link__container">
-                <Link className="nav-link__link" to={"/"}>
+                <Link
+                    className={pathname === "/" ? "nav-link__link nav-link__link-selected" : "nav-link__link"}
+                    to={"/"}
+                >
                     Home
                 </Link>
-                <Link className="nav-link__link" to={"/saved-arts"}>
+                <Link
+                    className={pathname === "/saved-arts" ? "nav-link__link nav-link__link-selected" : "nav-link__link"}
+                    to={"/saved-arts"}
+                >
                     Saved Arts
                 </Link>
             </ul>
