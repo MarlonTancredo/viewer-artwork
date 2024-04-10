223e4cd8334ca7f557a0e71794c6ad9e845de4c4
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-10 19:51:33 +0100

    feat: implmenting router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
+import "./main.css";
+
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./app/App";
-import { BrowserRouter } from "react-router-dom";
-import "./main.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <App />
-            </QueryClientProvider>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </StrictMode>,
 );
