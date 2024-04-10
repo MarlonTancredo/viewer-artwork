673d1d276877b476fe3f0e93497e7444f68d9a80
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-10 19:45:58 +0100

    feat: implmenting createBrowserRouter

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,18 +1,20 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
+import { createBrowserRouter, createRoutesFromElements, Route, Routes, ScrollRestoration } from "react-router-dom";
+
 import PageNotFound from "../pages/PageNotFound";
+import Home from "../pages/ArtWorks";
 import SavedArts from "../pages/SavedArts";
+import MainLayout from "../layout/MainLayout";
+import ArtWorks from "../pages/ArtWorks";
+import CardModal from "../components/CardModal";
 
-const Router = () => {
-    return (
-        <>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="/saved-arts" element={<SavedArts />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </>
-    );
-};
-
-export default Router;
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="art-works" element={<ArtWorks />} />
+            <Route path="card-modal" element={<CardModal />} />
+            <Route path="saved-arts" element={<SavedArts />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Route>,
+    ),
+);
