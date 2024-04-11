acd6a4ec2b2414b16ba95840a6d93e4518057831
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-11 19:21:54 +0100

    style: style card

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -27,38 +27,40 @@ const Cards = ({ data }: CardTypes) => {
                         url,
                     } = art;
                     return (
-                        <section id={id} key={id} className="card__body card--shadow card--fade-in">
+                        <section id={id} key={id} className="card__body container-shadow card--fade-in">
                             <img src={art.images.web.url} alt={title} className="card__img" />
                             <div className="card__description">
-                                <div className="card__title">
+                                <div className="card__title text--dark-color text--big text--bold">
                                     {title} {creation_date_latest}
                                 </div>
-                                <div className="card__creator">
+                                <div className="card__creator text--small text--medium-color">
                                     {creators.length === 0 ? "" : `By ${creators[0].description}`}
                                 </div>
-                                <div className="card__department">
-                                    <i>{department}</i>
+                                <div className="card__department text--small text--medium-color">
+                                    <i>({department})</i>
                                 </div>
-                                <Link
-                                    className="card__read-more"
-                                    to="/card-modal"
-                                    state={{
-                                        id: id,
-                                        title: title,
-                                        creation_date_latest: creation_date_latest,
-                                        department: department,
-                                        imgUrl: art.images.web.url,
-                                        accession_number: accession_number,
-                                        technique: technique,
-                                        type: type,
-                                        measurements: measurements,
-                                        did_you_know: did_you_know,
-                                        description: description,
-                                        linkToArtWork: url,
-                                    }}
-                                >
-                                    READ MORE...
-                                </Link>
+                                <div className="card__read-more__section ">
+                                    <Link
+                                        className="card__read-more text-link-color--blue text--medium"
+                                        to="/card"
+                                        state={{
+                                            id: id,
+                                            title: title,
+                                            creation_date_latest: creation_date_latest,
+                                            department: department,
+                                            imgUrl: art.images.web.url,
+                                            accession_number: accession_number,
+                                            technique: technique,
+                                            type: type,
+                                            measurements: measurements,
+                                            did_you_know: did_you_know,
+                                            description: description,
+                                            linkToArtWork: url,
+                                        }}
+                                    >
+                                        READ MORE...
+                                    </Link>
+                                </div>
                             </div>
                         </section>
                     );
