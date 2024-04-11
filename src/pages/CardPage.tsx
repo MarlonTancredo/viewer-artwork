6997a669f400bd6a41e337f2d1f5c806ecd83b65
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-11 19:25:29 +0100

    feat: implementing cardPage

---
+++ src/pages/CardPage.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import CardModal from "../components/CardModal";
+
+const CardPage = () => {
+    return (
+        <>
+            <div className="page__container page--fade-in ">
+                <main className="page__main">
+                    <CardModal />
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default CardPage;
