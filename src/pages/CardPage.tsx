2737da30d90e7ed5f012e0db47640d2631a6e7e9
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-20 00:48:44 +0100

    refactor: refactoring

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -1,11 +1,23 @@
 import "./styles.css";
 import CardModal from "../components/CardModal";
+import SavedCardModal from "../components/SavedCardModal";
+import { createContext, useState } from "react";
+
+export const CardPageContext = createContext<{
+    isSaved: boolean;
+    setIsSaved: React.Dispatch<React.SetStateAction<boolean>>;
+} | null>(null);
 
 const CardPage = () => {
+    const [isSaved, setIsSaved] = useState(false);
+
     return (
         <>
             <div className="page__container page--fade-in ">
-                <CardModal />
+                <SavedCardModal isModalActive={isSaved} />
+                <CardPageContext.Provider value={{ isSaved, setIsSaved }}>
+                    <CardModal />
+                </CardPageContext.Provider>
             </div>
         </>
     );
