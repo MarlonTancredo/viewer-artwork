cff01f8fcdbdaea6109f7d3116840d461d84a500
Marlon Tancredo <marlon_tancredo@hotmail.com>
2024-04-22 00:29:19 +0100

    refactor: refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "../routes/router";
+import { router } from "../routes/Router";
 import { createContext, useState } from "react";
 
 type InitialState = {
